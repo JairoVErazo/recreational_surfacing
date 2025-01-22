07ada184192e6ae88ff2949de5f032eb7ffc5f27
Jairo Erazo <92348509+LilianaAguilar14@users.noreply.github.com>
2025-01-21 20:29:10 -0600

    Se agrega Seo al sitio

---
+++ next-seo.config.js
@@ -0,0 +1,35 @@
+export default {
+    title: "Rubber Playground Safety Surfaces | Recreational Surfacing INC",
+    description:
+      "Discover innovative, safe, and sustainable playground safety surfaces with Recreational Surfacing INC. Explore options like Poured-in-Place Rubber, Synthetic Turf, and more.",
+    canonical: "https://recreational-surfacing-beta.vercel.app/",
+    openGraph: {
+      type: 'website',
+      locale: 'en_US',
+      url: 'https://recreational-surfacing-beta.vercel.app/',
+      site_name: 'Recreational Surfacing INC',
+      title: "Rubber Playground Safety Surfaces | Recreational Surfacing INC",
+      description:
+        "Recreational Surfacing INC provides safe, durable, and innovative surfaces for playgrounds, splash pads, fitness trails, and more. Experience quality and safety in every layer.",
+      images: [
+        {
+          url: 'https://recreational-surfacing-beta.vercel.app/img/logo.png', 
+          width: 1200,
+          height: 630,
+          alt: 'Rubber Playground Safety Surfaces for Endless Fun',
+        },
+      ],
+    },
+    additionalMetaTags: [
+      {
+        name: 'keywords',
+        content:
+          'recreational surfacing, playground safety surfaces, poured-in-place rubber, synthetic turf, bonded rubber mulch, equipment installation, splash pads, playground safety, ADA compliant surfaces',
+      },
+      {
+        name: 'author',
+        content: 'Recreational Surfacing INC',
+      },
+    ],
+  };
+  
