125944bf99afdd9fbb117c40f6e3741a49480a34
Jairo Erazo <92348509+LilianaAguilar14@users.noreply.github.com>
2025-03-30 20:45:44 -0600

    fix sitemap2

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://recreational-surfacing.com',
+    generateRobotsTxt: true, // Genera también un archivo robots.txt
+    sitemapSize: 5000, // Número máximo de URLs por sitemap
+  };
+  
